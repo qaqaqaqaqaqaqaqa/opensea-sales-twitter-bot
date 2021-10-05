0da75ef5a7ff6302fdd0b3b832b742ef6154f35d
nocluewhatimdoing <91919458+qaqaqaqaqaqaqaqa@users.noreply.github.com>
2021-10-04 23:36:16 -0400

    Update tweet.js
    ty for ur code Phil Marley I hope this shit works

--- tweet.js
+++ tweet.js
@@ -1,62 +1,107 @@
 const axios = require('axios');
 const twit = require('twit');
+const moment = require('moment');
 
 const twitterConfig = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
 
 const twitterClient = new twit(twitterConfig);
 
-// Tweet a text-based status
-async function tweet(tweetText) {
-    const tweet = {
-        status: tweetText,
-    };
 
-    twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
-        if (!error) {
-            console.log(`Successfully tweeted: ${tweetText}`);
-        } else {
-            console.error(error);
-        }
-    });
+// OpenSea doesn't give us access to Webhooks; need to poll every 60 seconds
+// Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
+
+async function handleDupesAndTweet(tokenName, tweetText, imageUrl) {
+    // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
+  twitterClient.get(
+    'search/tweets',
+    { q: tokenName, count: 1, result_type: 'recent' },
+    (error, data, response) => {
+      if (!error) {
+        const statuses = _.get(data, 'statuses');
+          
+          // No duplicate statuses found
+        if (_.isEmpty(data) || _.isEmpty(statuses)) {
+          console.log('No duplicate statuses found, continuing to tweet...');
+          return tweet(tweetText, imageUrl);
+        }          
+      
+        const mostRecentMatchingTweetCreatedAt = _.get(
+          statuses[0],
+          'created_at'
+        );
+        const statusOlderThan10Mins = moment(
+          mostRecentMatchingTweetCreatedAt
+        ).isBefore(moment().subtract(10, 'minutes'));
+        // Status found is older than 10 minutes, not a cached transaction, just sold at same price
+        if (statusOlderThan10Mins) {
+          console.log(
+            'Previous status is older than 10 minutes, continuing to tweet...'
+          );
+          
+           return tweet(tweetText, imageUrl);
+        }         
+          
+        console.error(
+          'Tweet is a duplicate; possible delayed transaction retrieved from OpenSea'
+        );
+      } else {
+        console.error(err);
+      }
+    }
+  );
 }
 
-// OPTIONAL - use this method if you want the tweet to include the full image file of the OpenSea item in the tweet.
-async function tweetWithImage(tweetText, imageUrl) {
-    // Format our image to base64
-    const processedImage = await getBase64(imageUrl);
-
-    // Upload the item's image from OpenSea to Twitter & retrieve a reference to it
-    twitterClient.post('media/upload', { media_data: processedImage }, (error, media, response) => {
-        if (!error) {
-            const tweet = {
-                status: tweetText,
-                media_ids: [media.media_id_string]
-            };
-
-            twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
-                if (!error) {
-                    console.log(`Successfully tweeted: ${tweetText}`);
-                } else {
-                    console.error(error);
-                }
-            });
-        } else {
-            console.error(error);
-        }
-    });
+// Upload image of item retrieved from OpenSea & then tweet that image + provided text
+async function tweet(tweetText, imageUrl) {
+    
+  // Format our image to base64
+  const processedImage = await getBase64(imageUrl);    
+    
+   // Upload the item's image from OpenSea to Twitter & retrieve a reference to it
+  twitterClient.post(
+    'media/upload',
+    { media_data: processedImage },
+    (error, media, response) => {
+      if (!error) {
+        const tweet = {
+          status: tweetText,
+          media_ids: [media.media_id_string],
+        };   
+    
+        twitterClient.post(
+          'statuses/update',
+          tweet,
+          (error, tweet, response) => {
+            if (!error) {
+              console.log(`Successfully tweeted: ${tweetText}`);
+            } else {
+              console.error(error);
+            }
+          }
+        );
+      } else {
+        console.error(error);
+      }
+    }
+  );
 }
 
 // Format a provided URL into it's base64 representation
 function getBase64(url) {
-    return axios.get(url, { responseType: 'arraybuffer'}).then(response => Buffer.from(response.data, 'binary').toString('base64'))
+    
+  return axios
+    .get(url, { responseType: 'arraybuffer' })
+    .then((response) =>
+      Buffer.from(response.data, 'binary').toString('base64')
+    );
 }
 
-module.exports = {
-    tweet: tweet,
-    tweetWithImage: tweetWithImage
+module.exports = {    
+    
+  handleDupesAndTweet: handleDupesAndTweet,
 };
